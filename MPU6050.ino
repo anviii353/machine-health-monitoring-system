58f6a8813efcc8c2cfc40fc4bd9248804b01b46a
MayurM1012 <mayurpmmh39@gmail.com>
2026-03-28 12:52:18 +0530

    Update MPU6050.ino

--- MPU6050.ino
+++ MPU6050.ino
@@ -1,56 +1,46 @@
+#include <Wire.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
-#include <Wire.h>
 
 Adafruit_MPU6050 mpu;
 
 void setup() {
-  Serial.begin(115200); // Make sure your Serial Monitor is set to 115200 baud!
-  
-  // Wait for serial monitor to open
-  while (!Serial) {
-    delay(10);
-  }
+  Serial.begin(115200);
+  Wire.begin(21, 22);  // SDA, SCL for ESP32
 
-  Serial.println("Initializing MPU6050...");
-
-  // Try to initialize the sensor over I2C
   if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip. Check your wiring!");
+    Serial.println("Failed to find MPU6050 chip");
     while (1) {
-      delay(10); // Halt the program if the sensor isn't found
+      delay(10);
     }
   }
-  Serial.println("MPU6050 Found and Connected!");
 
-  // Configure the sensor's sensitivity
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G); // Good for motor vibrations
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);   // Smooths out extreme noise
+  Serial.println("MPU6050 Found!");
+
+  // Optional: Set accelerometer range
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+
+  // Optional: Set filter bandwidth
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
   delay(100);
 }
 
 void loop() {
-  // Create variables to hold the sensor data
   sensors_event_t a, g, temp;
-  
-  // Fetch the data from the sensor
   mpu.getEvent(&a, &g, &temp);
 
-  // Print the Acceleration (Vibration) data 
-  // We format it this way specifically for the Arduino Serial Plotter
-  Serial.print("Vibration_X:");
+  Serial.print("Accel X: ");
   Serial.print(a.acceleration.x);
-  Serial.print(",");
-  
-  Serial.print("Vibration_Y:");
-  Serial.print(a.acceleration.y);
-  Serial.print(",");
-  
-  Serial.print("Vibration_Z:");
-  Serial.println(a.acceleration.z);
+  Serial.print(" m/s^2, ");
 
-  // Delay for 50 milliseconds (reads 20 times per second)
-  delay(50); 
+  Serial.print("Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.print(" m/s^2, ");
+
+  Serial.print("Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
+
+  delay(200);  // adjust for faster/slower readings
 }
